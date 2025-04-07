5efc1dd4235a198b7457b4e7be00d95165f39d0e
mariagorskikh <your.email@example.com>
2025-04-06 20:55:39 -0400

    Run server by directly executing the main file

--- bakery_api_simplified.py
+++ bakery_api_simplified.py
@@ -48,5 +48,6 @@ async def check_availability(query: str = Body(..., example="Can I order a crois
 
 # Main function to run everything
 if __name__ == "__main__":
-    print("Starting FastAPI server on http://localhost:8000")
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port) 
